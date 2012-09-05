dafb0e5b54ad3efcee74c41f8dde9259f57525f3
John Koht <john@kohactive.com>
2012-09-04 20:58:22 -0500

    Adding json success & error response methods and json_model(s) methods for eassy json outputs for models

--- lib/json_output_helper.rb
+++ lib/json_output_helper.rb
@@ -1,7 +1,11 @@
 module JsonOutputHelper
 
+
+
   # JSON helpers
+  # Simple methods for DRYing up and standardizing JSON output
   # ----------------------------------------------------------------------------------------------------
+  
   # a nice standard response schema for json
   def json_response(type, hash = {})
     # we require one of these types
@@ -15,8 +19,8 @@ module JsonOutputHelper
       :status => type, 
       :html => nil, 
       :notice => nil, 
-      :to => nil }.merge(hash)
-
+      :to => nil
+    }.merge(hash)
     return default_json
   end
 
@@ -24,6 +28,62 @@ module JsonOutputHelper
   def render_json_response(type, hash = {})
     render :json => json_response(type, hash)
   end
+  
+  
+  
+  
+  # Response specific helper: For Success and error handling
+  # ----------------------------------------------------------------------------------------------------
+  
+  # helper for success messages (for actions like delete which dont return models)
+  def json_success message, hash = {}
+    json = {
+      :success => true,
+      :message => message
+    }.merge(hash)
+    render :json => json
+  end
+
+  # helper for returning failure messages in a common format
+  def json_error code, message=nil, metadata={}
+    render :json => {
+      :error => metadata.merge({
+        :message => message || t("api.errors.#{code}"),
+        :code => code
+      })
+    }
+  end
+  
+  
+  
+  
+  # JSON Heper for Models
+  # ----------------------------------------------------------------------------------------------------
+  
+  # a standard way to return models. if they have errors then we return the error message
+  # this is a DRY approach to creating and updating and then returning JSON responses
+  def json_model model, extra_params={}
+    # By default, we use the api_attributes method to return model properties. A custom
+    # attributes parameter can be passed also
+    method = extra_params[:attributes] || "api_attributes"
+    if model.errors.empty?
+      render :json => model.send(method).deep_merge(extra_params)
+    else 
+      json_error :model_error, model.errors.first.join(' ')
+    end
+  end
+
+  # a standard way to return an array of models
+  # arrays of models are passed back in a data object, this is so we can add things we may need in the future such as pagination
+  def json_models models, extra_params={}
+    # By default, we use the api_attributes method to return model properties. A custom
+    # attributes parameter can be passed also
+    method = extra_params[:attributes] || "api_attributes"
+    render :json => models.collect{|model| model.send(method)} 
+  end
+  
+  
+  
 
 end
 
